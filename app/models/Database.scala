551554c59894c21e41d64450e919ea4eca0c9ab5
jilen <jilen.zhang@gmail.com>
2021-06-16 23:06:05 +0800

    use RIO.service

--- app/models/Database.scala
+++ app/models/Database.scala
@@ -10,18 +10,16 @@ object Database {
 
   trait Conn
 
-  def makeDB: RIO[console.Console, Database] = RIO.access[console.Console] { env =>
+  private def makeDB = RIO.service[console.Console.Service].map { _console =>
     new Database {
-      val _console = env.get[console.Console.Service]
       val makeConn = _console.putStrLn(s"Make conn").as(new Conn {}).orDie
-
-      def getConn = Managed.make(makeConn)(c => _console.putStrLn(s"Release conn: ${c}").orDie)
+      def getConn = Managed.make(makeConn)(c =>
+        _console.putStrLn(s"Release conn: ${c}").orDie
+      )
     }
   }
 
   def make: RManaged[console.Console, Database] = {
-    ZManaged.make(makeDB)(db =>
-      zio.console.putStrLn(s"$db closed").orDie
-    )
+    ZManaged.make(makeDB)(db => zio.console.putStrLn(s"$db closed").orDie)
   }
 }
